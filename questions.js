80500a7ba4023f46f26f0d90611704e965401944
Sarah Carter <sarahmarie.carter@yahoo.com>
2019-12-21 14:37:12 -0800

    created function that switches to next question when answer is selected

--- questions.js
+++ questions.js
@@ -25,3 +25,17 @@ let questions = [
         answer: "if(i == 5)"
     }
 ]
+
+// let questionEl = document.querySelector('#question');
+
+// let titles = [];
+// let selections = [];
+// for(let i = 0; i < questions.length; i++) {
+//     titles[i] = questions[i].title;
+//     selections[i] = questions[i].choices;
+// }
+
+// questionEl.textContent = questions[0].title;
+
+// console.log(titles);
+// console.log(selections);
--- script.js
+++ script.js
@@ -5,22 +5,20 @@
 //add click event to change to next question when answer = button
     //if userAnswer[i]=answer[i] displaymessage correct!
 
-//click on answer => i++
 
-// h1.textContent = questions title i
-
-//function that switches question when user presses button?
-
-//set attribute of list items to button
 
 let startButton = document.querySelector('#start');
 let quizEl = document.querySelector('.wrapper');
 let timerEl = document.querySelector('#time-left');
+let ulEl = document.querySelector('#answers');
+let questionEl = document.querySelector('#question');
+
+let questionIndex = 0;
+let timer = 10;
 
 quizEl.style.display = 'none';
 timerEl.style.display = 'none';
 
-let timer = 10;
 function startTimer() {
     setInterval(function() {
         timer--;
@@ -28,9 +26,31 @@ function startTimer() {
     }, 1000);
 }
 
+function nextQuestion() {
+    questionEl.textContent = questions[questionIndex].title;
+    for(let i = 0; i < 4; i++) {
+        let li = document.createElement('button');
+        li.textContent = questions[questionIndex].choices[i];
+        ulEl.appendChild(li);
+        console.log(li);
+    }
+    questionIndex++;
+}
+
+ulEl.addEventListener('click', function() {
+    ulEl.textContent = "";
+    nextQuestion();
+})
+
 startButton.addEventListener('click', function() {
     startButton.style.display = 'none';
     quizEl.style.display = 'block';
     timerEl.style.display = 'block';
     startTimer();
+    nextQuestion();
+})
+
+ulEl.addEventListener('click', function(event) {
+    let target = event.target;
+
 })
